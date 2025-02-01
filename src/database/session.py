533ea221e5f7bb294ce76b8d8baa3458dee803ad
oasilturk <moasilturk@gmail.com>
2025-02-01 21:48:38 +0300

    enhance db session

--- src/database/session.py
+++ src/database/session.py
@@ -1,7 +1,18 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from contextlib import contextmanager
-from src.database.database import SessionLocal
+import os
 
-@contextmanager
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@postgres/backend_db")
+
+# Set up the database engine
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# ✅ Correct way to return a database session
 def get_db():
     db = SessionLocal()
     try:
